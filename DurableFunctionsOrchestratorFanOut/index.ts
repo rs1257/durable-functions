20ced65a992b416f3dfced866fa5c4f802eb5f38
Ryan Smith <ryan.smith1@bp.com>
2022-11-10 10:59:16 +0000

    Renamed existing to chaining, added a fan out example

---
+++ DurableFunctionsOrchestratorFanOut/index.ts
@@ -0,0 +1,16 @@
+﻿import * as df from "durable-functions";
+
+const orchestrator = df.orchestrator(function* (context) {
+  const parallelTasks = [];
+  const workBatch = ["Tokyo", "Seattle", "London"];
+
+  for (const workItem of workBatch) {
+    parallelTasks.push(context.df.callActivity("Hello", workItem));
+  }
+
+  yield context.df.Task.all(parallelTasks);
+
+  return parallelTasks.map((task) => task.result);
+});
+
+export default orchestrator;
